723d8f5948b6d5522192b48a3f1b4e72a4d1d1a2
Nestor R Benavidez <nestor.benavidez@gmail.com>
2019-05-20 14:03:18 -0300

    Clean up assets, create dashboads and links to customers and projects, re route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get 'pages/dashboard'
   get 'pages/login'
+  root :to => "pages#dashboard"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
